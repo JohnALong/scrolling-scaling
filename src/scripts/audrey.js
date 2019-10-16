ca4bd6e895de0ed73404fdb68f444aa8d71b3260
John Long <jlcomp45@gmail.com>
2019-10-16 13:29:33 -0500

    scroll scale exercise - not completed

--- src/scripts/audrey.js
+++ src/scripts/audrey.js
@@ -4,7 +4,8 @@ const audrey = document.getElementById("audrey")
     Add an event listener to the `document` object to listen
     for the "scroll" event.
 */
-.addEventListener("", function () {
+
+document.addEventListener("scroll", function () {
     /*
         Adjust the width of audrey to be 1/3 the value of
         `window.scrollY`. No lower than 50px, though.
