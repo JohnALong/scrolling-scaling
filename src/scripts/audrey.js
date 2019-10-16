b5e8fd7cd27412938abbcda18d5341bffe1112f6
John Long <jlcomp45@gmail.com>
2019-10-16 15:42:34 -0500

    completed scrolling scaling exercise

--- src/scripts/audrey.js
+++ src/scripts/audrey.js
@@ -6,6 +6,17 @@ const audrey = document.getElementById("audrey")
 */
 
 document.addEventListener("scroll", function () {
+    if (window.scrollY >= 50) {
+        audrey.style.width = `${window.scrollY / 3}px`;
+        console.log("width", audrey.style.width)
+    } else {
+        audrey.style.width = "50px";
+    }
+    if (window.scrollY >= 100) {
+        audrey.style.height = `${window.scrollY * .25}px`;
+    } else {
+        audrey.style.height = "100px";
+    }
     /*
         Adjust the width of audrey to be 1/3 the value of
         `window.scrollY`. No lower than 50px, though.
@@ -17,4 +28,5 @@ document.addEventListener("scroll", function () {
         Adjust the height of audrey to be 1/4 the value of
         `window.scrollY`. No lower than 100px, though.
     */
+
 })
